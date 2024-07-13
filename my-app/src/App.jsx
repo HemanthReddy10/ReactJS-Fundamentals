12d5e85a1f984fc0ae7fb74819d63c5c0d979aec
HemanthReddy10 <yaramala.h@northeastern.edu>
2024-07-13 10:29:50 -0400

    added images

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import './App.css'
 function App() {
   return (
-    <div>
-      Javascript XML
+    <div className='container'>
+      <p className='para'>This is Paragraph</p>
+      <img className='hero-img'src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg" alt="net error" />
+      <img className='hero-img'src="PictureBox/img1.jpg" alt="" />
+      {/* my-app/public/PictureBox/img1.jpg */}
     </div>
   )
 }
