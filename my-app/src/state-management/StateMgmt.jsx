ef0d55e4b9dc2620e591c31877eae39b2b73e56b
HemanthReddy10 <yaramala.h@northeastern.edu>
2024-07-13 17:13:38 -0400

    useEffect

--- my-app/src/state-management/StateMgmt.jsx
+++ my-app/src/state-management/StateMgmt.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 
 export const StateMgmt = () => {
     const[city,setCity]=useState("Hyderabad");
     // setCity("Vijayawada"); wrong approach
+
+    // if(city==="Goa"){
+    //     setCity("Bangalore");
+    // }
+    // else{
+    //     setCity("Delhi");
+    // }
+    // error: React limits the number of renders to prevent an infinite loop
+    useEffect(()=>{
+       if(city==="Hyderabad"){
+        setCity("Bangalore"); //rerenders again 
+    }
+    else{
+        setCity("Delhi"); // rerenders again , but dom will not update since value is already delhi
+        // setState is asynchronous ,only updates when it detects the change
+    }
+    },[city])
+
   return (
-    <div>{city}</div>
+    <div>I live in {city}</div>
   )
 }
