bfcfc97f42dadf6f504a2a3d9496eeb5942b5775
HemanthReddy10 <yaramala.h@northeastern.edu>
2024-07-13 21:04:06 -0400

    toastify

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,40 +1,37 @@
-import React,{useState} from 'react'
-import './App.css'
-import FirstComp from './props-container/FirstComp'
-import ThirdComp from './props-container/ThirdComp'
-import FourthComp from './props-container/FourthComp'
-import { StateMgmt } from './state-management/StateMgmt'
-import { ClickEvent } from './ClickEvent'
-import ScreenSize from './state-management/ScreenSize'
-import FormExample from './FormExample'
+import React, { useState } from "react";
+import "./App.css";
+import FirstComp from "./props-container/FirstComp";
+import ThirdComp from "./props-container/ThirdComp";
+import FourthComp from "./props-container/FourthComp";
+import { StateMgmt } from "./state-management/StateMgmt";
+import { ClickEvent } from "./ClickEvent";
+import ScreenSize from "./state-management/ScreenSize";
+import FormExample from "./FormExample";
 
-
-const car={
-  model:"tesla",
-  year:2024,
-  dealer:"musk",
-  color:"silver"
-}
+const car = {
+  model: "tesla",
+  year: 2024,
+  dealer: "musk",
+  color: "silver",
+};
 function App() {
   // eslint-disable-next-line
-  const[sampleCondition,setSampleCondition]=useState(true);
-
- 
-
+  const [sampleCondition, setSampleCondition] = useState(true);
   return (
-    <div className='container'>
-     <FirstComp car={car}/>
-     <StateMgmt/>
-     <br/>
-     { sampleCondition? <FourthComp name="hemanth"/> : <ThirdComp/>}
-     <br/>
-     <ClickEvent/>
-     <br/>
-     <ScreenSize/>
-     <hr/>
-     <FormExample/>
+    <div className="container">
+      <FirstComp car={car} />
+      <StateMgmt />
+      <br />
+      {sampleCondition ? <FourthComp name="hemanth" /> : <ThirdComp />}
+      <br />
+      <ClickEvent />
+      <br />
+      <ScreenSize />
+      <hr />
+      <FormExample />
+      <hr />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-app/src/FormExample.jsx
+++ my-app/src/FormExample.jsx
@@ -1,33 +1,40 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const FormExample = () => {
-    const[userName,setUserName]=useState("");
-    const[newUserDetails,setNewUserDetails]=useState("")
-    const getUserName=(e)=>{
+  const [userName, setUserName] = useState("");
+  const [newUserDetails, setNewUserDetails] = useState("");
+  const notify = () => toast("Data uploaded successfully!");
+  const getUserName = (e) => {
+    setUserName(e.target.value);
+  };
 
-         setUserName(e.target.value)
-    }
-
-    const userDetails=(e)=>{
-        e.preventDefault();
-      setNewUserDetails(userName)
-    }
+  const userDetails = (e) => {
+    e.preventDefault();
+    setNewUserDetails(userName);
+    notify()
+  };
   return (
     <section className="form-section">
-       
-        <div className='form-input'>
+      <div className="form-input">
         <h2>Hello {newUserDetails}</h2>
-            <form onSubmit={userDetails}>
-            
-            <input type="text" placeholder='Enter your name' onChange={getUserName} />
-            <br/>
-            <button className='submit-btn' type='submit'>Submit</button>
-            </form>
-        </div>
-
+        <ToastContainer/>
+        <form onSubmit={userDetails}>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            onChange={getUserName}
+          />
+          <br />
+          <button className="submit-btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default FormExample
+export default FormExample;
